dd8a928298825e725cf86f5127e698cae8196226
Haebin <joker7011@naver.com>
2024-03-15 03:42:14 +0900

    feat: member -> user 엔티티 이름 변경

--- src/main/kotlin/com/vacgom/backend/domain/member/Member.kt
+++ src/main/kotlin/com/vacgom/backend/domain/member/Member.kt
@@ -4,13 +4,18 @@ import com.vacgom.backend.global.auditing.BaseEntity
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "t_user")
-class User(
-        val nickname: String
-) : BaseEntity() {
+@Table(name = "t_member")
+class Member(nickname: String) : BaseEntity() {
 
     @Id
-    @Column(name = "user_id")
+    @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
+
+    @Column(name = "nickname")
+    val nickname: String
+
+    init {
+        this.nickname = nickname
+    }
 }
