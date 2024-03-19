15090dc238ca084b4b4e689769a29333e782022f
Haebin <joker7011@naver.com>
2024-03-20 07:55:18 +0900

    feat: Security Logger 개선

--- src/main/kotlin/com/vacgom/backend/global/security/filter/JwtAuthFilter.kt
+++ src/main/kotlin/com/vacgom/backend/global/security/filter/JwtAuthFilter.kt
@@ -6,6 +6,7 @@ import com.vacgom.backend.global.security.model.CustomUser
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.Logger
 import org.springframework.security.authentication.AnonymousAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
@@ -15,7 +16,8 @@ import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class JwtAuthFilter(
-        private val jwtService: JwtService
+        private val jwtService: JwtService,
+        private val log: Logger
 ) : OncePerRequestFilter() {
     companion object {
         private const val HEADER_AUTHORIZATION = "Authorization"
@@ -37,12 +39,12 @@ class JwtAuthFilter(
             val authenticationToken = AnonymousAuthenticationToken("guest", principal, setOf(authority))
             SecurityContextHolder.getContext().authentication = authenticationToken
             chain.doFilter(request, response)
-            println("principal = ${principal}")
+            log.info("{}({}) - ({}) {}", principal.username, principal.authorities, request.method, request.requestURL)
             return
         }
         val authentication = jwtService.getAuthentication(token)
         SecurityContextHolder.getContext().authentication = authentication
-        println("authentication = ${authentication}")
+        log.info("{}", SecurityContextHolder.getContext())
         chain.doFilter(request, response)
     }
 
