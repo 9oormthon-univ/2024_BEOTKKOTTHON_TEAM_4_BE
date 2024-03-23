879056634e02f34d1a95a8f45aa51e9015781385
Haebin <joker7011@naver.com>
2024-03-23 18:29:33 +0900

    feat: 멤버 실명 응답 스펙 추가

--- src/main/kotlin/com/vacgom/backend/auth/application/dto/response/MeResponse.kt
+++ src/main/kotlin/com/vacgom/backend/auth/application/dto/response/MeResponse.kt
@@ -5,6 +5,7 @@ import com.vacgom.backend.member.domain.Member
 class MeResponse(
     val id: String,
     val nickname: String?,
+    val name: String,
     val healthConditions: List<HealthConditionResponse>,
 ) {
     companion object {
@@ -12,6 +13,7 @@ class MeResponse(
             return MeResponse(
                 id = member.id.toString(),
                 nickname = member.nickname?.nickname,
+                name = member.memberDetails!!.name,
                 healthConditions = member.healthProfiles.map { HealthConditionResponse.of(it.healthCondition) },
             )
         }
