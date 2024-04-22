690c1dc32e06bc263b0b6e335cc38a533f12fd46
HyungJu <sungkisa@naver.com>
2024-04-22 22:12:53 +0900

    fix: health check endpoint를 authEndpoints에 추가

--- src/main/kotlin/com/vacgom/backend/global/security/matcher/CustomRequestMatcher.kt
+++ src/main/kotlin/com/vacgom/backend/global/security/matcher/CustomRequestMatcher.kt
@@ -7,26 +7,26 @@ import org.springframework.stereotype.Component
 
 @Component
 class CustomRequestMatcher {
-
     fun authEndpoints(): RequestMatcher {
         return OrRequestMatcher(
-                AntPathRequestMatcher("/"), // Actuator Health Checker
-                AntPathRequestMatcher("/api/v1/oauth/**"), // Oauth Login
-                AntPathRequestMatcher("/api/v1/member/validation"), // VacgomId Validation Checker
-                AntPathRequestMatcher("/api/v1/event"), // VacgomId Validation Checker
+            AntPathRequestMatcher("/"), // Actuator Health Checker
+            AntPathRequestMatcher("/api/v1/oauth/**"), // Oauth Login
+            AntPathRequestMatcher("/api/v1/member/validation"), // VacgomId Validation Checker
+            AntPathRequestMatcher("/api/v1/event"), // VacgomId Validation Checker
+            AntPathRequestMatcher("/actuator/health"), // VacgomId Validation Checker
         )
     }
 
     fun tempUserEndpoints(): RequestMatcher {
         return OrRequestMatcher(
-                AntPathRequestMatcher("/api/v1/member/signup")
+            AntPathRequestMatcher("/api/v1/member/signup"),
         )
     }
 
     fun userEndpoints(): RequestMatcher {
         return OrRequestMatcher(
-                AntPathRequestMatcher("/api/v1/inoculation/**"),
-                AntPathRequestMatcher("/api/v1/search/**"),
+            AntPathRequestMatcher("/api/v1/inoculation/**"),
+            AntPathRequestMatcher("/api/v1/search/**"),
         )
     }
 }
