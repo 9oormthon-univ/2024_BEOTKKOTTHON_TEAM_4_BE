18f84b789757a723a9f76929c8fb429446721ee4
HyungJu <sungkisa@naver.com>
2024-05-12 22:48:09 +0900

    feat: 회원가입시 각종 알림을 전송

--- src/main/kotlin/com/vacgom/backend/auth/application/VacgomSignupService.kt
+++ src/main/kotlin/com/vacgom/backend/auth/application/VacgomSignupService.kt
@@ -5,6 +5,7 @@ import com.vacgom.backend.auth.application.dto.response.AuthResponse
 import com.vacgom.backend.auth.application.dto.response.MemberResponse
 import com.vacgom.backend.auth.application.dto.response.TokenResponse
 import com.vacgom.backend.auth.domain.constants.Role
+import com.vacgom.backend.disease.domain.constants.AgeCondition
 import com.vacgom.backend.global.exception.error.BusinessException
 import com.vacgom.backend.global.security.jwt.JwtFactory
 import com.vacgom.backend.inoculation.domain.Inoculation
@@ -17,6 +18,8 @@ import com.vacgom.backend.member.exception.MemberError
 import com.vacgom.backend.member.exception.NicknameError
 import com.vacgom.backend.member.infrastructure.persistence.HealthProfileRepository
 import com.vacgom.backend.member.infrastructure.persistence.MemberRepository
+import com.vacgom.backend.notification.application.NotificationService
+import com.vacgom.backend.search.application.SearchService
 import jakarta.transaction.Transactional
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
@@ -31,6 +34,8 @@ class VacgomSignupService(
     private val vaccinationRepository: VaccinationRepository,
     private val log: Logger,
     private val jwtFactory: JwtFactory,
+    private val notificationService: NotificationService,
+    private val searchService: SearchService,
 ) {
     fun validateNickname(id: String) {
         val nickname = Nickname(id)
@@ -86,6 +91,41 @@ class VacgomSignupService(
 
         inoculationRepository.saveAll(inoculations)
         member.addInoculations(inoculations)
+
+        val age = Calendar.getInstance().get(Calendar.YEAR) - memberDetails.birthday.year
+        val ageCondition =
+            when (age) {
+                in 19..29 -> AgeCondition.AGE19TO29
+                in 30..39 -> AgeCondition.AGE30TO39
+                in 40..49 -> AgeCondition.AGE40TO49
+                in 50..59 -> AgeCondition.AGE50TO59
+                in 60..69 -> AgeCondition.AGE60TO64
+                else -> AgeCondition.AGEOVER65
+            }
+
+        val recommendedDiseases =
+            searchService.searchDisease(
+                listOf(ageCondition),
+                healthConditions,
+            )
+
+        notificationService.sendNotification(
+            memberId,
+            "${member.nickname}님을 위한 백신이 ${recommendedDiseases.size}개 있어요!",
+            "vaccine",
+        )
+
+        notificationService.sendNotification(
+            memberId,
+            "${member.nickname}님의 접종 인증서가 ${inoculations.size}개 발급됐어요",
+            "vaccine",
+        )
+
+        notificationService.sendNotification(
+            memberId,
+            "${member.nickname}님! 백곰 가입을 환영해요 :)",
+            "success",
+        )
         return AuthResponse(true, memberResponse, tokenResponse)
     }
 }
